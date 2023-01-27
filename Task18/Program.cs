64b40e39227229736ba1fb21b847478ec05efd8f
Алёна Алёна <alenchaeto1@outlook.com>
2023-01-27 09:22:25 +0300

    Добавьте файлы проекта.

---
+++ Task18/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            Console.WriteLine($"Количество скобок корректно? {Check(str)}");
+            Console.ReadKey();
+        }
+        static bool Check(string str)
+        {
+
+            Stack<char> stack = new Stack<char>();
+            Dictionary<char, char> brackets = new Dictionary<char, char>
+            {
+                {'(',')'},
+                {'{','}'},
+                {'[',']'},
+            };
+
+            foreach (char c in str)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(brackets[c]);
+                }
+
+                if (c == ')' || c == '}' || c == ']')
+                {
+                    if (stack.Count == 0 || stack.Pop() != c)
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (stack.Count == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
